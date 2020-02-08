bb176cec3500970981c65891e19ee3e91438c4c5
takuma <Homies05.inc@gmail.com>
2020-02-08 23:32:39 +0900

    migration tables

---
+++ database/migrations/2020_02_08_215055_create_threads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('university_id');
+            $table->string('class_id');
+            $table->string('grade_id');
+            $table->string('title');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
