f345ca09269b4a5c020a6e59236373e5eed880a4
takuto <Homies05.inc@gmail.com>
2020-03-05 19:34:52 +0900

    [update] cssの更新
    時間割表のリンク範囲を拡大、app.cssを複製、style.cssを作成”

--- resources/views/schedule/detail.blade.php
+++ resources/views/schedule/detail.blade.php
@@ -8,35 +8,30 @@
                 <div class="panel-heading">{{ config('time.' . $id) }} 授業情報</div>
                 
                 <div class="panel-body">
-                    <label>授業名</label>
-                        <p>{{ $lecture->name }}</p>
-                    <label>教室</label>
-                        <p>{{ $lecture->room_number }}</p>
-                    <label>担当教員</label>
-                        <p>{{ $lecture->teacher }}</p>
+                    <label>授業名</label><p>{{ $lecture->name }}</p>
+                    <label>教室</label><p>{{ $lecture->room_number }}</p>
+                    <label>担当教員</label><p>{{ $lecture->teacher }}</p>
                     <br>
                     <br>
                     <br>
                     <br>
-                    ここに掲示板をくっつけるイメージ
-
                     @if (session('message'))
-                    <div class="alert alert-success">{{session('message')}}</div>
+                        <div class="alert alert-success">{{session('message')}}</div>
                     @endif
 
                     @if (session('error'))
-                    <div class="alert alert-danger">{{session('error')}}</div>
+                        <div class="alert alert-danger">{{session('error')}}</div>
                     @endif
 
                     <!--  ここをcssで投稿がなくても掲示板の形を取るようにする -->
 
                     @if ($posts->isNotEmpty())
                         <table>
-                        @foreach ($posts as $post)
-                        <tr>
-                            <td>{{$post->body}} 投稿者{{$post->user->name}}</td>
-                        </tr>
-                        @endforeach
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{$post->body}} 投稿者{{$post->user->name}}</td>
+                                </tr>
+                            @endforeach
                         </table>   
                     @else
 
@@ -45,7 +40,7 @@
                     <form method="post" action="{{ route('class.store') }}">
                         {{ csrf_field() }}
                         <input type="hidden" name="class_id" value="{{$id}}">
-                        <input type="text" name="body">
+                        <input type="text" name="body" class="form-control">
                         <input type="submit" value="投稿する">
                     </form>
                 </div>
