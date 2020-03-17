ef974ea6e13382f7e8eb8f6638f3a343ada03592
takuto <Homies05.inc@gmail.com>
2020-03-17 14:58:47 +0900

    [update] 時間割、掲示板のデザイン修正
    時間割のセルの幅を修正（※授業名などの文字数にバリデージョンをかける必要あり）
    掲示板の表示形式を変更
    掲示板の英語の長い文字列の折り返しを実装
    ナビバーの設置

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -4,41 +4,54 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <!--<div class="panel-heading">
-                    時間割
-                </div>-->
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <table border=1>
-                        <tr>
-                            <th></th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th>
-                        </tr>
-                        <!-- 以下の部分は改善の余地あり -->
-                        @for ($i = 1; $i <= 36; $i++)
-                            @if ($i % 6 == 1)
-                                <tr><td>○限</td>
-                            @endif
-                            <td>
-                                @if ($lecture_infos[$i] == null)
-                                    <a href="{{ route('schedules.create', ['id' => $i]) }}">{{ '-' }}</a>
-                                @else
-                                    <a href="{{ route('class.show', ['id' => $i]) }}">{{ $lecture_infos[$i]['name'] }}<br>{{ $lecture_infos[$i]['room_number'] }}</a>
-                                @endif
-                            </td>
-                            @if ($i % 6 == 0)
-                                </tr>
-                            @endif
-                        @endfor
-                    </table>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+            <table border=1>
+                <tr>
+                    <th bgcolor="#dce6f5" width="10%"></th>
+                    <th bgcolor="#dce6f5" width="15%">月</th>
+                    <th bgcolor="#dce6f5" width="15%">火</th>
+                    <th bgcolor="#dce6f5" width="15%">水</th>
+                    <th bgcolor="#dce6f5" width="15%">木</th>
+                    <th bgcolor="#dce6f5" width="15%">金</th>
+                    <th bgcolor="#dce6f5" width="15%">土</th>
+                </tr>
+                @php
+                    $j = 1;
+                @endphp
+                <!-- 以下の部分は改善の余地あり -->
+                @for ($i = 1; $i <= 36; $i++)
+                    @if ($i % 6 == 1)
+                        <tr><td bgcolor="#dce6f5" width="4%">{{ $j++ }}</td>
+                    @endif
+                    <td width="16%">
+                        @if ($lecture_infos[$i] == null)
+                            <a href="{{ route('schedules.create', ['id' => $i]) }}">{{ '-' }}</a>
+                        @else
+                            <font size="1" color="#7e8183">
+                                <a href="{{ route('class.show', ['id' => $i]) }}">{{ $lecture_infos[$i]['name'] }}<br>{{ $lecture_infos[$i]['room_number'] }}</a>
+                            </font>
+                        @endif
+                    </td>
+                    @if ($i % 6 == 0)
+                        </tr>
+                    @endif
+                @endfor
+            </table>
         </div>
     </div>
 </div>
+<nav class="navbar navbar-default navbar-fixed-bottom">
+    <font size="1" color="#7e8183">
+        <ul class="nav nav-pills" style="text-align: center">
+            <li role="presentation" style="width: 24%"><a href="#">マイページ</a></li>
+            <li role="presentation" style="width: 25%" class="active"><a href="{{ route('schedules.index') }}">時間割</a></li>
+            <li role="presentation" style="width: 25%"><a href="#">大学掲示板</a></li>
+            <li role="presentation" style="width: 24%"><a href="#">お知らせ</a></li>
+        </ul>
+    </font>
+</nav>
 @endsection
