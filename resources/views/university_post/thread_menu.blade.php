906572dff9958a28b93a3ea163a7f870a3aa86ab
takuto <Homies05.inc@gmail.com>
2020-03-29 02:24:13 +0900

    [update] 大学全体掲示板の見た目の修正
    configに掲示板の種類（授業、サークルなど）、軽い説明を持たせた。
    表示形式を他のページと統一

--- resources/views/university_post/thread_menu.blade.php
+++ resources/views/university_post/thread_menu.blade.php
@@ -12,14 +12,17 @@
                 <div class="alert alert-success">{{session('error')}}</div>
             @endif
             <input id="lefile" type="file" style="display:none">
-            
-            {{ config($pref_id . '.' . $university_id .'.name') }}の全体掲示板
-            <div class="panel panel-default">
-                <div class="list-group">
+            <div class="panel panel-primary">
+                <div class="panel-heading" style="text-align:center">
+                    {{ config($pref_id . '.' . $university_id .'.name') }}の全体掲示板
+                </div>
                     @for ($i = 1; $i <= 4; $i++)
-                        <a class="list-group-item" href="{{ route('threads.show', ['id' => $i]) }}">
-                            <p class="list-group-item-heading">{{ config('thread.' . $i) }}</p>
-                        </a>
+                        <div class="list-group" style="text-align:center;">
+                            <a class="list-group-item" href="{{ route('threads.show', ['id' => $i]) }}">
+                                <p class="list-group-item-heading" style="font-size:16px">{{ config('thread.' . $i . '.name') }}</p>
+                                <p style="font-size:11px">{{ config('thread.' . $i . '.detail') }}</p>
+                            </a>
+                        </div>
                     @endfor
                 </div>
             </div>
